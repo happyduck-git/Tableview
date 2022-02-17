919130e1581c66954741989353c304517dfb7cb7
GG <rkrudtls@gmail.com>
2022-02-17 13:43:19 +0900

    Build basic form of TableView programmatically.

---
+++ TableView/SimpleTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SimpleTableViewCell.swift
+//  TableView
+//
+//  Created by HappyDuck on 2022/02/17.
+//
+
+import UIKit
+
+class SimpleTableViewCell: UITableViewCell {
+    
+    static let identifier = "SimpleTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var sampleArray = ["Airplane mode", "Wi-Fi", "Bluetooth", "Cellular", "Personal Hotspot", "VPN"]
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(SimpleTableViewCell.self, forCellReuseIdentifier: SimpleTableViewCell.identifier)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SimpleTableViewCell.identifier, for: indexPath)
+        cell.textLabel?.text = sampleArray[indexPath.row]
+        return cell
+    }
+
 
 
 }
